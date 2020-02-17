07f14da801ae4d12c6be95407117b4692d796474
Mohamed Ghouse <53009059+itsmohamedghouse@users.noreply.github.com>
2020-02-17 21:42:39 +0530

    Add files via upload

--- 1-triangle.py
+++ 1-triangle.py
@@ -1,14 +1,21 @@
-print("Enter three sides of triangle: \n")
-a = int(input())
-b = int(input())
-c = int(input())
-if a > 10 or b >10 or c > 10:
-	print("Enter values less than 10")
-elif ((a + b > c) and (a + c > b) and (b + c > a)) :
+# Design and develop a program in a language of your choice to solve the triangle problem
+# defined as follows: Accept three integers which are supposed to be the three sides of a
+# triangle and determine if the three values represent an equilateral triangle, isosceles triangle,
+# scalene triangle, or they do not form a triangle at all. Derive test cases for your program
+# based on decision-table approach, execute the test cases and discuss the results.
+
+
+a = int(input("Enter first side of triangle:"))
+b = int(input("Enter second sides of triangle:"))
+c = int(input("Enter third sides of triangle:"))
+
+# Check if triangle is valid
+if ((a + b > c) and (a + c > b) and (b + c > a)) :
 	print("Triangle is valid.")
+	# if valid check the type of triangle
 	if a == b == c:
 		print("Equilateral triangle")
-	elif a==b or b==c or c==a:
+	elif (a==b or b==c or c==a):
 		print("isosceles triangle")
 	else:
 		print("Scalene triangle")
